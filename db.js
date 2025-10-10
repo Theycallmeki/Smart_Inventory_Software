ff74353d0fd7363db26a7ee74149f2ba83a33020
Theycallmeki <laxamana.kian13@gmail.com>
2025-10-11 07:10:34 +0800

    2

--- db.js
+++ db.js
@@ -1,18 +1,12 @@
-// db.js
 const { Sequelize } = require('sequelize');
 
-// PostgreSQL connection using Render credentials
 const sequelize = new Sequelize(
-  'defense_6r8n',                // Database name
-  'defense_6r8n_user',           // Username
-  '8bD5nLRSkdCAVJI5vR3cmoKmEqqx8cII', // Password
+  'postgresql://database_2eqz_user:zUjzeeJBk8Bjkjn46tthHmohxM9Phxsa@dpg-d3kp392dbo4c73a76c4g-a.singapore-postgres.render.com/database_2eqz',
   {
-    host: 'dpg-d3knmuemcj7s7387po6g-a.singapore-postgres.render.com', // External host
-    port: 5432,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
-        require: true,          // SSL required for Render
+        require: true,
         rejectUnauthorized: false,
       },
     },
